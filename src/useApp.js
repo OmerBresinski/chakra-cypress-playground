3a2c010740429df75e825f42ec947e602fc016e7
Omer Bresinski <omerbresinski@Omers-MacBook-Pro.local>
2021-12-28 17:24:44 +0200

    Added smooth scrolling

--- src/useApp.js
+++ src/useApp.js
@@ -40,7 +40,10 @@ const useApp = () => {
   };
 
   const scrollToTop = () => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
 
   return {
